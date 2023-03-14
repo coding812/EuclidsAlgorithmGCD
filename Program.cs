50393bd323a8373201f35edfb6b7e5598237493e
coding812 <85715642+coding812@users.noreply.github.com>
2023-03-14 09:57:33 -0400

    Update Program.cs
    formatted output

--- Program.cs
+++ Program.cs
@@ -1,24 +1,38 @@
-﻿Console.WriteLine("Give me two numbers and I'll give you their greatest common divisor!");
-Console.Write("First number: ");
-int a = int.Parse(Console.ReadLine());
-Console.Write("Second number: ");
-int b = int.Parse(Console.ReadLine());
-
-// Gets remainder of when "a" is divided by "b" and assigns it to "r"
-int r = a % b;
-
-// ensures "a" is always the larger number
-if (b > a)
+﻿internal class Program
 {
-    a = b; b = a;
-}
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Give me two numbers and I'll give you their greatest common divisor!");
+        Console.Write("First number: ");
+        int a = int.Parse(Console.ReadLine());
+        Console.Write("Second number: ");
+        int b = int.Parse(Console.ReadLine());
 
-// As long as "r" is not equal to zero, repeats Euclid's method for finding GCD
-// If "r" does equal zero, loop never runs and skips to printing "b" as the GCD
-while (r != 0)
-{
-    a = b; b = r;
-    r = a % b;
-}
+        int result = GreatestCommonDivisor(a, b);
 
-Console.WriteLine(b);
+        Console.WriteLine(string.Format("The Greatest Common Divisor of {0} and {1} is {2}.", a, b, result));
+    }
+
+    private static int GreatestCommonDivisor(int a, int b)
+    {
+        // Gets remainder of when "a" is divided by "b" and assigns it to "r"
+        int r = a % b;
+
+        // ensures "a" is always the larger number
+        if (b > a)
+        {
+            a = b; b = a;
+        }
+
+        // As long as "r" is not equal to zero, repeats Euclid's method for finding GCD
+        // If "r" does equal zero, loop never runs and skips to printing "b" as the GCD
+        while (r != 0)
+        {
+            a = b; b = r;
+            r = a % b;
+        }
+
+        int divisor = b;
+        return divisor;
+    }
+}
